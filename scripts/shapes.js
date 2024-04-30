b905671b8bbb40d133192dfe2757bbd3096bb14c
frenzie24 <kevincharlesgross@gmail.com>
2024-04-30 14:08:02 -0400

    added render to shape class added logic to draw svg shapes in each child class of shape

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -1,29 +1,28 @@
+// nl is a string literal for a line return
+const nl = `
+`;
 
 class Square extends Shape {
     constructor(color, text, textColor) {
-        super(color,  text, textColor) ;
-        this.x = 55;
-        this.y = 10;
-        this.rWidth = 190;
-        this.rHeight = 190;
-
+        super(color, text, textColor);
+        this.shapeEle = `<rect width="300" height="200" x="0" y="0"  fill="${color}" />`;
+        this.textEle = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" stroke=${this.textColor} fill=${this.textColor} font-size="90" >${this.text}</text>`
     }
 }
 
 class Cirlce extends Shape {
     constructor(color, text, textColor) {
         super(color, text, textColor);
-        this.cx = "150";
-        this.cy="100";
-        this.r='95';
-        
+        this.shapeEle = `<circle r="35%" cx="50%" cy="50%" fill="${color}" />`
+        this.textEle = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" stroke=${this.textColor} fill=${this.textColor} font-size="70" >${this.text}</text>`
     }
 }
 
 class Triangle extends Shape {
     constructor(color, text, textColor) {
         super(color, text, textColor);
-        this.points="10,190 280,190 150,10" 
+        this.shapeEle = ` <polygon points="0,200 300,200 150,0" style="fill:${color}" />`
+        this.textEle = `<text x="50%" y="66%" dominant-baseline="middle" text-anchor="middle" fill=${this.textColor} font-size="60" stroke=${this.textColor} stroke-width=1 >${this.text}</text>`
     }
 }
 
@@ -32,11 +31,12 @@ class Shape {
         this.fill = color;
         this.text = text;
         this.textColor = textColor;
-        this.width = 300;
-        this.height = 200;
+        this.svgString = `<svg width="300 height="200 xmlns="http//www.w3.org/2000/svg>`;
+        this.textEle = ``;
+        this.shapeEle = ``;
     }
 
-    drawShape () {
-
+    render() {
+        return `${this.svgString}${this.shapeEle}${this.textEle}</svg>`
     }
 }
